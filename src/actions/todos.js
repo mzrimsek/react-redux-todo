63ece2c2ad95f21140aa142fab1ab9dc62a46642
Mike Zrimsek <mikezrimsek@gmail.com>
2017-02-27 10:17:38 -0500

    extract contants to constants file

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -1,7 +1,9 @@
+import { ADD_TODO, TOGGLE_TODO } from '../constants';
+
 let nextTodoId = 0;
 export const addTodo = (text) => {
   return {
-    type: 'ADD_TODO',
+    type: ADD_TODO,
     id: nextTodoId++,
     text
   };
@@ -9,7 +11,7 @@ export const addTodo = (text) => {
 
 export const toggleTodo = (id) => {
   return {
-    type: 'TOGGLE_TODO',
+    type: TOGGLE_TODO,
     id
   };
 };
---
+++ src/constants.js
@@ -0,0 +1,11 @@
+// todo actions
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+
+// visibility filter actions
+export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
+
+// visibility filter action options
+export const SHOW_ALL = "SHOW_ALL";
+export const SHOW_ACTIVE = "SHOW_ACTIVE";
+export const SHOW_COMPLETED = "SHOW_COMPLETED";
