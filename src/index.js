9182843d2f30a45cd7afd87880f472a7624483f8
Mike Zrimsek <mikezrimsek@gmail.com>
2017-03-01 12:06:33 -0500

    added middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
 
+const logger = store => next => action => {
+    console.log('dispatching', action);
+    let result = next(action);
+    console.log('next state', store.getState());
+    return result;
+};
+
+const crashReporter = store => next => action => {
+    try {
+        return next(action);
+    }
+    catch (err) {
+        console.error('Caught an exception!', err);
+        Raven.captureException(err, {
+            extra: {
+                action,
+                state: store.getState()
+            }
+        });
+        throw err;
+    }
+};
+
 let store = createStore(
     todoApp,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(logger, crashReporter)
 );
 
 render(
